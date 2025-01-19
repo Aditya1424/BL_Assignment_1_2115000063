4bcfefdc07c8ca2b59667ecb78a8a36ad4bb1ce7
Aditya Gupta <ag7023587@gmail.com>
2025-01-19 17:59:20 +0530

    Add two numbers code added

---
+++ add_two_numbers.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class add_two_numbers{
+	public static void main(String []args){
+		Scanner adi= new Scanner(System.in);
+		int a=adi.nextInt();
+		int b= adi.nextInt();
+		
+		int res= a+b;
+		System.out.println(a+b);
+	}
+}
